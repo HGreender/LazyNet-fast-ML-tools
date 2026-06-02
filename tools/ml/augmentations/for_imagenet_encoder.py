266b18f164002ab7bc5b845a2635101a1f134ab1
ktv <kostyanov@ftizisbiomed.ru>
2026-06-02 11:08:17 +0300

    Add func for fast base augmentations with prepared parameters

---
+++ tools/ml/augmentations/for_imagenet_encoder.py
@@ -0,0 +1,41 @@
+import cv2
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD = [0.229, 0.224, 0.225]
+
+
+def get_imagenet_encoder_augmentation(
+        phase='train',
+        size=(512, 512)
+):
+    """Аугментации, важные для медицинских снимков"""
+    if phase == 'train':
+        return A.Compose([
+            A.Resize(size[0], size[1]),
+            A.HorizontalFlip(p=0.5),
+            A.Affine(
+                translate_percent=0.05,
+                scale=(0.9, 1.1),
+                rotate=(-15, 15),
+                border_mode=cv2.BORDER_CONSTANT,
+                p=0.5
+            ),
+            A.OneOf([
+                A.GaussNoise(std_range=(0.1, 0.2)),
+                A.MotionBlur(blur_limit=3),
+            ], p=0.2),
+            A.OneOf([
+                A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2),
+                A.CLAHE(clip_limit=2.0, tile_grid_size=(8, 8), p=0.5),
+            ], p=0.2),
+            A.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+            ToTensorV2(),
+        ])
+    else:
+        return A.Compose([
+            A.Resize(size[0], size[1]),
+            A.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+            ToTensorV2(),
+        ])
