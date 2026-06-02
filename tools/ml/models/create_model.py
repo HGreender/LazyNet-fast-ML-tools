9dbc8223cab1cd12e6b287a8f6f29acd2dfa4575
ktv <kostyanov@ftizisbiomed.ru>
2026-06-02 11:18:27 +0300

    Add funcs for creating base medicine models

---
+++ tools/ml/models/create_model.py
@@ -0,0 +1,31 @@
+import segmentation_models_pytorch as smp
+
+
+def create_efficientnetb4_unet(classes: list = ['target_class'], activation: str | None = None):
+    """
+    Создание UNet с EfficientNet-B4
+    decoder_channels: каналы в декодере. Для B4 обычно используют уменьшенные каналы для экономии памяти.
+    """
+    model = smp.Unet(
+        encoder_name='efficientnet-b4',
+        encoder_weights='imagenet',
+        classes=len(classes),
+        activation=activation,
+        decoder_channels=(256, 128, 64, 32, 16), # Стандарт для тяжелых энкодеров
+    )
+    return model
+
+
+def create_efficientnetb4_unetpp(classes: list = ['target_class'], activation: str | None = None):
+    """
+    Создание UNet++ с EfficientNet-B4
+    decoder_channels: каналы в декодере. Для B4 обычно используют уменьшенные каналы для экономии памяти.
+    """
+    model = smp.UnetPlusPlus(
+        encoder_name='efficientnet-b4',
+        encoder_weights='imagenet',
+        classes=len(classes),
+        activation=activation,
+        decoder_channels=(256, 128, 64, 32, 16), # Стандарт для тяжелых энкодеров
+    )
+    return model
