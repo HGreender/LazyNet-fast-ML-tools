09a0a9b17f42d984a18390d1489219ac089e79b8
ktv <kostyanov@ftizisbiomed.ru>
2026-06-08 15:37:31 +0300

    Fix torchmetrics params

--- fast_ml_tools/ml/metrics/segmentation.py
+++ fast_ml_tools/ml/metrics/segmentation.py
@@ -23,7 +23,7 @@ def get_segmentation_metrics(metric_names: list = None):
         "Dice": DiceScore(
             num_classes=2,           # Фон (0) и Объект (1)
             average="macro",         # Усреднение по классам (рекомендуется)
-            input_format="one-hot"   # Ожидает тензоры формы [B, C, H, W]
+            input_format="index"   # Ожидает тензоры формы [B, C, H, W]
         )
     }
 
