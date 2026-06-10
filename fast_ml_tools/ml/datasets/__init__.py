51ed9b6247a6f9fbf39f3b80253edcd58b91b36e
ktv <kostyanov@ftizisbiomed.ru>
2026-06-10 14:22:19 +0300

    Del old classes and their imports

--- fast_ml_tools/ml/datasets/__init__.py
+++ fast_ml_tools/ml/datasets/__init__.py
@@ -1,11 +1,7 @@
-from .from_dirs import DirsDataset
-from .split_dirs import create_train_val_datasets
 from .multi_dirs import create_train_val_datasets_from_multiple_dirs, MultiDirsDataset
 
 
 __all__ = [
-    "DirsDataset",
     "MultiDirsDataset",
-    "create_train_val_datasets",
     "create_train_val_datasets_from_multiple_dirs",
 ]
