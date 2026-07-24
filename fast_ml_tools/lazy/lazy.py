4b5d7ebdeb693a37b9c4b87332174b4df39ecbe3
ktv <timurkostyan@gmail.com>
2026-07-24 16:30:01 +0300

    Change default for dynamic_axes

--- fast_ml_tools/lazy/lazy.py
+++ fast_ml_tools/lazy/lazy.py
@@ -248,8 +248,8 @@ class LazyNet:
         # Определяем динамические оси (опционально)
         if dynamic_axes is None:
             dynamic_axes = {
-                'input': {0: 'batch_size', 2: 'height', 3: 'width'},
-                'output': {0: 'batch_size', 2: 'height', 3: 'width'}
+                'input': {0: 'batch_size'},
+                'output': {0: 'batch_size'}
             }
 
         # Создаем директорию если нужно
