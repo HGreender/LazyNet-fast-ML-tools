431e707eb31a8d874885aa5d1d77b8213c7b32de
ktv <timurkostyan@gmail.com>
2026-08-07 11:10:45 +0300

    fix: Fix new params

--- fast_ml_tools/ml/augmentations/for_imagenet_encoder.py
+++ fast_ml_tools/ml/augmentations/for_imagenet_encoder.py
@@ -23,7 +23,7 @@ def get_imagenet_encoder_augmentation(
             ),
             A.Resize(size[0], size[1]),
             A.OneOf([
-                A.GaussNoise(std_range=(10.0, 50.0)),
+                A.GaussNoise(std_range=(0.02, 0.1)),
                 A.GaussianBlur(blur_limit=(3, 5)),
             ], p=0.2),
             A.OneOf([
@@ -72,7 +72,7 @@ def get_lungs_imagenet_encoder_augmentation(
             ], p=0.34),
             A.Resize(size[0], size[1]),
             A.OneOf([
-                A.GaussNoise(std_range=(10.0, 50.0)),
+                A.GaussNoise(std_range=(0.02, 0.1)),
                 A.GaussianBlur(blur_limit=(3, 7)),
             ], p=0.2),
             A.OneOf([
