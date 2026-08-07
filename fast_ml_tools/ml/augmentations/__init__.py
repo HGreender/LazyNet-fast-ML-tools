4cd11785d41c5645555e8552d9e10fba7d72d753
ktv <timurkostyan@gmail.com>
2026-08-07 10:26:43 +0300

    feat: Add init for lungs segmentation augs

--- fast_ml_tools/ml/augmentations/__init__.py
+++ fast_ml_tools/ml/augmentations/__init__.py
@@ -1,5 +1,7 @@
-from .for_imagenet_encoder import get_imagenet_encoder_augmentation
+from .for_imagenet_encoder import (get_imagenet_encoder_augmentation,
+                                   get_lungs_imagenet_encoder_augmentation)
 
 __all__ = [
     "get_imagenet_encoder_augmentation",
+    "get_lungs_imagenet_encoder_augmentation"
 ]
